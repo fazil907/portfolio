78d74b1ab651a68d2502dd71048fb40b5caca21f
fazil907 <fazilbasheer36@gmail.com>
2023-04-14 13:07:23 +0530

    new1

--- contact-form.js
+++ contact-form.js
@@ -1,46 +1,55 @@
-
-    function validation(){
+ function validation(){
     var name = document.getElementById('name').value
     var email = document.getElementById('email').value
     var message = document.getElementById('message').value
-    
+    var nameValidation = /^[A-Za-z]+$/;
+    reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    email_result = reg.test(email);
 
     if(name == ''){
-      document.getElementById('name_error').innerHTML = "please enter your name"
+      alert("Enter a valid name please");
       return false
-    }else if(name.length<3){
-      document.getElementById('name_error').innerHTML = " please enter a valid name"
+    }else if(name.length<=3){
+      alert("The  name must be atleast 3 character")
+      return false
+    } else if(name.indexOf(" ") >= 0){
+      alert("Name not entered")
+      return false
+    } else if (!nameValidation.test(name) ) {
+      alert("Invalid characters");
       return false
     }
     else{
-      document.getElementById('name_error').innerHTML = ""
+      document.getElementById('name').innerHTML=""
       name = true
     }
 
    
     if(email == ''){
-      document.getElementById('email_error').innerHTML = "please enter your email"
+      alert("Please enter your correct email")
       return false
-    }else if(email.length<5){
-      document.getElementById('email_error').innerHTML = " please enter a valid email"
+    }else if(email.length<8){
+      alert("Please enter your correct E-mail")
+      return false
+    } else if(!email_result){
+      alert("Please provide a correct E-mail")
       return false
     }
     else{
-      document.getElementById('email_error').innerHTML = ""
+      document.getElementById('email').innerHTML = ""
       email = true
     }
 
 
     if(message == ''){
-      document.getElementById('message_error').innerHTML = "please enter any message"
+      alert("Message must not be Blank")
       return false
     }else if(message.length<10){
-      document.getElementById('message_error').innerHTML = " please enter atleast 10 characters"
+      alert("Message must be more than 10 char")
       return false
     }
     else{
-      document.getElementById('message_error').innerHTML = ""
+      document.getElementById('message').innerHTML = ""
       message = true
-      // setTimeout(function() {window.location = "./page2/done.html" });
     }
-  }
+}
